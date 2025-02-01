a33a4edfc8c9c07e4a360a165aac53d1959023f7
vagrant <vagrant@myubuntu>
2025-02-01 17:32:49 +0000

    Separated interface from class

--- src/ModeleCinema.java
+++ src/ModeleCinema.java
@@ -1,168 +1,140 @@
-
-public interface IModeleCinema {
-  public Utilisateur getUtilisateur (int id);
-  public Film getFilm (int id);
-  public Reservation getReservation (id: int);
-  public Salle getSalle (numero: int);
-  public Seance getSeance (id: int);
-  public Set<Utilisateur> getListeUtilisateur ();
-  public Set<Film> getListeFilms ();
-  public Set<Salle> getListeSalles ();
-  public Set<Seance> getListeSeancesFilm (f: Film);
-  public Set<Seance> getListeSeances ();
-  public int ajouterClient (nom: String, email: String, mdp: String);
-  public int ajouterManager (nom: String, email: String, mdp: String);
-  public int ajouterFilm (titre: String, a: int, desc: String);
-  public int ajouterFilm (titre: String, a: int, desc: String, genres: ArrayList<String>);
-  public boolean supprimerFilm (id: int);
-  public boolean ajouterSalle (num: int, capacite: int);
-  public boolean supprimerSalle (numero: int);
-  public boolean ajouterSeance (s: Seance);
-  public boolean supprimerSeance (id: int);
-  public boolean creerReservation (ArrayList<Seance>);
-  public boolean supprimerReservation (id: int);
-  public ArrayList<boolean> connecterUtilisateur (String email, String mdp);
-  public boolean deconnecterUtilisateur (String email);
-}
-
-
 public class ModeleCinema implements IModeleCinema {
-  private Utilisateur utilisateurConnecte;
-  private Set<Utilisateur> utilisateursEnregistres;
-  private Set<Film> filmsEnregistres;
-  private Set<Salle> sallesEnregistrees;
-  private Set<Seance> seancesEnregistrees;
-  
-  /**
-  * Renvoie l'utilisateur enregistré dont l'id est donné en paramètre, s'il existe
-  * Renvoie null sinon
-  */
-  public Utilisateur getUtilisateur (int id) {
-    for (Utilisateur u : this.utilisateursEnregistres) {
-      if (u.getId() == id) {
-        return u;
-      }
-    }
-    return null;
-  }
-  /**
-  * Renvoie le film enregistré dont l'id est donné en paramètre, s'il existe
-  * Renvoie null sinon
-  */
-  public Film getFilm (int id) {
-    for (Film f : this.utilisateursEnregistres) {
-      if (f.getId() == id) {
-        return f;
-      }
-    }
-    return null;
-  }
-  /**
-  * Renvoie la réservation enregistrée dont l'id est donné en paramètre, si elle existe
-  * Renvoie null sinon
-  */
-  public Reservation getReservation (int id) {
-    for (Reservation r : this.utilisateursEnregistres) {
-      if (r.getId() == id) {
-        return r;
-      }
-    }
-    return null;
-  }
-  public Salle getSalle (int numero) {
-    for (Salle s : this.utilisateursEnregistres) {
-      if (s.getNumero() == numero) {
-        return s;
-      }
-    }
-    return null;
-  }
-  public Seance getSeance (int id) {
-    ;
-  }
-  public Set<Utilisateur> getListeUtilisateur () {
-    ;
-  }
-  public Set<Film> getListeFilms () {
-    ;
-  }
-  public Set<Salle> getListeSalles () {
-    ;
-  }
-  public Set<Seance> getListeSeancesFilm (f: Film) {
-    ;
-  }
-  public Set<Seance> getListeSeances () {
-    ;
-  }
-  public int ajouterClient (nom: String, email: String, mdp: String) {
-    ;
-  }
-  public int ajouterManager (nom: String, email: String, mdp: String) {
-    ;
-  }
-  public int ajouterFilm (titre: String, a: int, desc: String) {
-    ;
-  }
-  public int ajouterFilm (titre: String, a: int, desc: String, genres: ArrayList<String>) {
-    ;
-  }
-  public boolean supprimerFilm (id: int) {
-    ;
-  }
-     public boolean ajouterSalle(int num, int capacite) {
-        return sallesEnregistrees.add(new Salle(num, capacite));
-    }
+	private Utilisateur utilisateurConnecte;
+	private Set<Utilisateur> utilisateursEnregistres;
+	private Set<Film> filmsEnregistres;
+	private Set<Salle> sallesEnregistrees;
+	private Set<Seance> seancesEnregistrees;
+	
+	/**
+	* Renvoie l'utilisateur enregistré dont l'id est donné en paramètre, s'il existe
+	* Renvoie null sinon
+	*/
+	public Utilisateur getUtilisateur (int id) {
+		for (Utilisateur u : this.utilisateursEnregistres) {
+			if (u.getId() == id) {
+				return u;
+			}
+		}
+		return null;
+	}
+	/**
+	* Renvoie le film enregistré dont l'id est donné en paramètre, s'il existe
+	* Renvoie null sinon
+	*/
+	public Film getFilm (int id) {
+		for (Film f : this.utilisateursEnregistres) {
+			if (f.getId() == id) {
+				return f;
+			}
+		}
+		return null;
+	}
+	/**
+	* Renvoie la réservation enregistrée dont l'id est donné en paramètre, si elle existe
+	* Renvoie null sinon
+	*/
+	public Reservation getReservation (int id) {
+		for (Reservation r : this.utilisateursEnregistres) {
+			if (r.getId() == id) {
+				return r;
+			}
+		}
+		return null;
+	}
+	public Salle getSalle (int numero) {
+		for (Salle s : this.utilisateursEnregistres) {
+			if (s.getNumero() == numero) {
+				return s;
+			}
+		}
+		return null;
+	}
+	public Seance getSeance (int id) {
+		;
+	}
+	public Set<Utilisateur> getListeUtilisateur () {
+		;
+	}
+	public Set<Film> getListeFilms () {
+		;
+	}
+	public Set<Salle> getListeSalles () {
+		;
+	}
+	public Set<Seance> getListeSeancesFilm (f: Film) {
+		;
+	}
+	public Set<Seance> getListeSeances () {
+		;
+	}
+	public int ajouterClient (nom: String, email: String, mdp: String) {
+		;
+	}
+	public int ajouterManager (nom: String, email: String, mdp: String) {
+		;
+	}
+	public int ajouterFilm (titre: String, a: int, desc: String) {
+		;
+	}
+	public int ajouterFilm (titre: String, a: int, desc: String, genres: ArrayList<String>) {
+		;
+	}
+	public boolean supprimerFilm (id: int) {
+		;
+	}
+		 public boolean ajouterSalle(int num, int capacite) {
+				return sallesEnregistrees.add(new Salle(num, capacite));
+		}
 
-    public boolean supprimerSalle(int numero) {
-        return sallesEnregistrees.removeIf(s -> s.getNumero() == numero);
-    }
+		public boolean supprimerSalle(int numero) {
+				return sallesEnregistrees.removeIf(s -> s.getNumero() == numero);
+		}
 
-    public boolean ajouterSeance(Seance s) {
-        return seancesEnregistrees.add(s);
-    }
+		public boolean ajouterSeance(Seance s) {
+				return seancesEnregistrees.add(s);
+		}
 
-    public boolean supprimerSeance(int id) {
-        return seancesEnregistrees.removeIf(s -> s.getIdSeance() == id);
-    }
+		public boolean supprimerSeance(int id) {
+				return seancesEnregistrees.removeIf(s -> s.getIdSeance() == id);
+		}
 
 public boolean creerReservation(ArrayList<Seance> seances) {
-        for (Seance s : seances) {
-            if (s.getPlacesRestantes() > 0) {
-                s.ajouterReservation(new Reservation(utilisateurConnecte, s));
-            } else {
-                return false;
-            }
-        }
-        return true;
-    }
+				for (Seance s : seances) {
+						if (s.getPlacesRestantes() > 0) {
+								s.ajouterReservation(new Reservation(utilisateurConnecte, s));
+						} else {
+								return false;
+						}
+				}
+				return true;
+		}
 
-    public boolean supprimerReservation(int id) {
-        for (Seance s : seancesEnregistrees) {
-            if (s.supprimerReservation(id)) {
-                return true;
-            }
-        }
-        return false;
-    }
+		public boolean supprimerReservation(int id) {
+				for (Seance s : seancesEnregistrees) {
+						if (s.supprimerReservation(id)) {
+								return true;
+						}
+				}
+				return false;
+		}
 
-    public boolean connecterUtilisateur(String email, String mdp) {
-        for (Utilisateur u : utilisateursEnregistres) {
-            if (u.getMail().equals(email) && u.verifierMotDePasse(mdp)) {
-                utilisateurConnecte = u;
-                return true;
-            }
-        }
-        return false;
-    }
+		public boolean connecterUtilisateur(String email, String mdp) {
+				for (Utilisateur u : utilisateursEnregistres) {
+						if (u.getMail().equals(email) && u.verifierMotDePasse(mdp)) {
+								utilisateurConnecte = u;
+								return true;
+						}
+				}
+				return false;
+		}
 
-    public boolean deconnecterUtilisateur(String email) {
-        if (utilisateurConnecte != null && utilisateurConnecte.getMail().equals(email)) {
-            utilisateurConnecte = null;
-            return true;
-        }
-        return false;
-    }
+		public boolean deconnecterUtilisateur(String email) {
+				if (utilisateurConnecte != null && utilisateurConnecte.getMail().equals(email)) {
+						utilisateurConnecte = null;
+						return true;
+				}
+				return false;
+		}
 }
 
 }
