2a1ded3744b85cf675390c1a23efb05bab243cc0
vagrant <vagrant@myubuntu>
2025-02-02 13:24:02 +0000

    Préparation des tests

--- src/TestModele.java
+++ src/TestModele.java
@@ -3,5 +3,13 @@ public class TestModele {
     ModeleCinema monModele = new ModeleCinema();
     
     // À tester : ?
+    // ajouter/get/getListe
+    // []
+    // Client/Manager
+    // Billets/BilletExtra
+    // Salle
+    // Seance, TypeSeance
+    // Film, Genre
+    // Reservation
   }
 }
