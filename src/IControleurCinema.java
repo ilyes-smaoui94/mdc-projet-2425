a494f8e8dc6940d64708e4e7040ddc26495c3b68
vagrant <vagrant@myubuntu>
2025-02-02 20:18:50 +0000

    IControleur

--- src/IControleurCinema.java
+++ src/IControleurCinema.java
@@ -1,3 +1,60 @@
 public interface IControleurCinema {
+	// Setters and Gettersa
+	public void setVues(IVueClient vueClient, IVueManager vueManager);
 
+	public IVueClient getVueClient();
+
+	public IVueManager getVueManager();
+
+	public void setModele(IModeleCinema modeleCinema);
+
+	public IModeleCinema getModele();
+
+	// Application Management
+	public void lancerApplication();
+
+	public void gererConnexion();
+
+	// Reservation and Film Management
+	public void gererReservationSeance();
+
+	public void gererAffichageFilm();
+
+	public void gererAffichageFilms();
+
+	public void gererAffichageSalles();
+
+	public void gererAffichageToutesSeances();
+
+	public void gererAffichageSeancesUnFilm();
+
+	public void gererAffichageUneSeance();
+
+	public void gererAffichageResa();
+
+	public void gererNouvelleResa();
+
+	public void gererSuppressionResa();
+
+	// User Management
+	public void gererCreationUtilisateur();
+
+	public void gererSuppressionUtilisateur();
+
+	public void gererDeconnexion();
+
+	// Film Management
+	public void gererCreationFilm();
+
+	public void gererSuppressionFilm();
+
+	// Salle Management
+	public void gererCreationSalle();
+
+	public void gererSuppressionSalle();
+
+	// Seance Management
+	public void gererCreationSeance();
+
+	public void gererSuppressionSeance();
 }
