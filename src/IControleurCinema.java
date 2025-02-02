c2bb929b95c5c224189d68e75351ac234d133cf1
vagrant <vagrant@myubuntu>
2025-02-02 13:27:02 +0000

    Pour les tests, je vais juste garder les classes relatives au Modèle

--- src/IControleurCinema.java
+++
@@ -1,5 +0,0 @@
-public interface IControleurCinema {
-  // public void ajouterUtilisateur (String nom, String email, String mdp);
-  // public void connect (String email, String mdp);
-  // public void disconnect ();
-}
