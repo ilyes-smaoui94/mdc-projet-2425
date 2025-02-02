a83af3fca1cf9e5cfe8725bbdf2c323c64672c99
ArthurLGoff <73652216+ArthurLGoff@users.noreply.github.com>
2025-02-02 18:32:28 +0100

    Update SupplementPopcorn.java

--- src/SupplementPopcorn.java
+++ src/SupplementPopcorn.java
@@ -1,12 +1,52 @@
+/**
+ * Représente un supplément popcorn pour un billet, ajoutant un coût 
+ * additionnel de 6.0€ au prix de base du billet associé.
+ *
+ * <p>
+ * Cette classe hérite de {@link BilletExtra}, qui sert de décorateur 
+ * dans un pattern Decorator pour un objet {@link Billet}. 
+ * Le prix final est donc la somme du prix du billet de base 
+ * et d'un supplément de 6.0€.
+ * </p>
+ *
+ * <p>Exemple d'utilisation :
+ * <pre>{@code
+ * Seance seance = new Seance(...);
+ * Billet billetEtudiant = new BilletEtudiant(seance);
+ * Billet supplementPopcorn = new SupplementPopcorn(seance, billetEtudiant);
+ *
+ * double prixFinal = supplementPopcorn.getPrix();
+ * System.out.println("Prix final avec supplément popcorn : " + prixFinal + "€");
+ * }</pre>
+ * </p>
+ * 
+ * @see Billet
+ * @see BilletExtra
+ * @see Seance
+ */
 public class SupplementPopcorn extends BilletExtra {
 
-	public SupplementPopcorn(Seance seanceBillet, Billet billetBase) {
-		super(seanceBillet, billetBase);
-	}
-	
-	public double getPrix() {
-		// TODO Auto-generated method stub
-		double p = this.getBilletBase().getPrix();
-		return p + 6.0;
-	}
+    /**
+     * Construit un nouveau supplément popcorn pour un billet spécifique.
+     *
+     * @param seanceBillet La séance associée au billet.
+     * @param billetBase   Le billet de base sur lequel appliquer le supplément.
+     */
+    public SupplementPopcorn(Seance seanceBillet, Billet billetBase) {
+        super(seanceBillet, billetBase);
+    }
+
+    /**
+     * Calcule et retourne le prix du billet incluant le supplément popcorn.
+     * <p>
+     * Le prix final est le prix du billet de base majoré de 6.0€.
+     * </p>
+     *
+     * @return Le prix total du billet avec supplément popcorn.
+     */
+    @Override
+    public double getPrix() {
+        double p = this.getBilletBase().getPrix();
+        return p + 6.0;
+    }
 }
